430e20127262755925aeec7a81db4bd71bbcc80a
rejas <github@veeck.de>
2015-03-28 13:14:23 +0100

    move src into own directory

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,13 @@ var gulp            = require('gulp'),
     stylish         = require('jshint-stylish');
 
 gulp.task('csslint', function () {
-    return gulp.src('imagelightbox.css')
+    return gulp.src('src/imagelightbox.css')
         .pipe(csslint('.csslintrc'))
         .pipe(csslint.reporter())
 });
 
 gulp.task('minify:css', function () {
-    return gulp.src('imagelightbox.css')
+    return gulp.src('src/imagelightbox.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions', 'ie >= 7', 'Firefox ESR', 'Android >= 2.3'],
             cascade: false
@@ -25,13 +25,13 @@ gulp.task('minify:css', function () {
 });
 
 gulp.task('jshint', function () {
-    return gulp.src('imagelightbox.js')
+    return gulp.src('src/imagelightbox.js')
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
 });
 
 gulp.task('minify:js', function () {
-    return gulp.src('imagelightbox.js')
+    return gulp.src('src/imagelightbox.js')
         .pipe(uglify())
         .pipe(rename('imagelightbox.min.js'))
         .pipe(gulp.dest('dist/'));
