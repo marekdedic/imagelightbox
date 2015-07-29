01026255c908f2cea5465123d00846eacec95510
rejas <github@veeck.de>
2015-07-29 22:18:37 +0200

    remove now unnecessary opera mobile hack

--- src/imagelightbox.js
+++ src/imagelightbox.js
@@ -312,9 +312,7 @@
 
         if( options.quitOnDocClick )
         {
-            // fix the bug , using the opera (moblie) and wechat. 
-            $( document ).on('click', function( e )
-            // $( document ).on( hasTouch ? 'touchend' : 'click', function( e )
+            $( document ).on( hasTouch ? 'touchend' : 'click', function( e )
             {
                 if( image.length && !$( e.target ).is( image ) )
                 {
