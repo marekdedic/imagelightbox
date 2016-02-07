56d6d978b6ee5563e97398461df56049ee1469ef
Chris <chrisdavidramos@gmail.com>
2016-02-06 21:54:18 -0500

    Navigation typo fix
    Fixed typo that prevented 'navigation' option from working.

--- src/auxiliary.js
+++ src/auxiliary.js
@@ -2,20 +2,25 @@ var light = (function ($) {
     "use strict";
 
     var activityIndicatorOn = function () {
+        console.log("ILB: activityIndicatorOn()");
         $('<div id="imagelightbox-loading"><div></div></div>').appendTo('body');
     },
         activityIndicatorOff = function () {
+            console.log("ILB: activityIndicatorOff()");
             $('#imagelightbox-loading').remove();
         },
 
         overlayOn = function () {
+            console.log("ILB: overlayOn()");
             $('<div id="imagelightbox-overlay"></div>').appendTo('body');
         },
         overlayOff = function () {
+            console.log("ILB: overlayOff()");
             $('#imagelightbox-overlay').remove();
         },
 
         closeButtonOn = function (instance) {
+            console.log("ILB: closeButtonOn()");
             $('<a href="#" id="imagelightbox-close"></a>').appendTo('body').on('click', function () {
                 $(this).remove();
                 instance.quitImageLightbox();
@@ -23,19 +28,23 @@ var light = (function ($) {
             });
         },
         closeButtonOff = function () {
+            console.log("ILB: closeButtonOff()");
             $('#imagelightbox-close').remove();
         },
 
         captionOn = function () {
+            console.log("ILB: captionOn()");
             var description = $('a[href="' + $('#imagelightbox').attr('src') + '"] img').attr('alt');
             if (description.length > 0)
                 $('<div id="imagelightbox-caption">' + description + '</div>').appendTo('body');
         },
         captionOff = function () {
+            console.log("ILB: captionOff()");
             $('#imagelightbox-caption').remove();
         },
 
         navigationOn = function (instance, selector) {
+            console.log("ILB: navigationOn()");
             var images = $(selector);
             if (images.length) {
                 var nav = $('<div id="imagelightbox-nav"></div>');
@@ -64,14 +73,18 @@ var light = (function ($) {
             }
         },
         navigationUpdate = function (selector) {
+            console.log("ILB: navigationUpdate()");
+            console.log("ILB: navigationUpdate()");
             var items = $('#imagelightbox-nav').find('a');
             items.removeClass('active');
             items.eq($(selector).filter('[href="' + $('#imagelightbox').attr('src') + '"]').index(selector)).addClass('active');
         },
         navigationOff = function () {
+            console.log("ILB: navigationOff()");
             $('#imagelightbox-nav').remove();
         },
         arrowsOn = function( instance, selector ) {
+            console.log("ILB: arrowsOn()");
             var $arrows = $( '<button type="button" class="imagelightbox-arrow imagelightbox-arrow-left"></button>' +
                              '<button type="button" class="imagelightbox-arrow imagelightbox-arrow-right"></button>' );
             $arrows.appendTo( 'body' );
@@ -90,6 +103,7 @@ var light = (function ($) {
             });
         },
         arrowsOff = function() {
+            console.log("ILB: arrowsOn()");
             $('.imagelightbox-arrow').remove();
         };
 
@@ -139,7 +153,7 @@ var light = (function ($) {
             Bar.init(input[0],
                      input[1].activity,
                      input[1].arrows,
-                     input[1].naviatiion,
+                     input[1].navigation,
                      input[1].overlay,
                      input[1].caption,
                      input[1].button
@@ -147,4 +161,3 @@ var light = (function ($) {
     };
     
 })(jQuery);
-
