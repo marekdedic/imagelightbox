5156650d83169f79c6fb0163344081a7aa1c9fb2
hanwiz <hanwiz@gmail.com>
2016-05-20 11:13:55 -0500

    Fix the issue in ios to align the image vertically.

--- src/imagelightbox.js
+++ src/imagelightbox.js
@@ -269,7 +269,8 @@
                 }
 
                 var screenWidth = $(window).width() * 0.8,
-                    screenHeight = $(window).height() * 0.9,
+                    wHeight = (window.innerHeight) ? window.innerHeight : $(window).height(),                    
+                    screenHeight = wHeight * 0.9,
                     tmpImage = new Image();
 
                 tmpImage.src = image.attr('src');
@@ -280,13 +281,15 @@
                     if (imageWidth > screenWidth || imageHeight > screenHeight) {
                         var ratio = imageWidth / imageHeight > screenWidth / screenHeight ? imageWidth / screenWidth : imageHeight / screenHeight;
                         imageWidth /= ratio;
+                        imageWidth = Math.floor(imageWidth);
                         imageHeight /= ratio;
+                        imageHeight = Math.floor(imageHeight);
                     }
 
                     image.css({
                         'width': imageWidth + 'px',
                         'height': imageHeight + 'px',
-                        'top': ( $(window).height() - imageHeight ) / 2 + 'px',
+                        'top': ( wHeight - imageHeight ) / 2 + 'px',
                         'left': ( $(window).width() - imageWidth ) / 2 + 'px'
                     });
                 };
