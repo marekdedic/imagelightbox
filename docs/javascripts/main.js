2c9f93af9c8973567a707adff3ced0b975b2dbd6
Veeck <github@veeck.de>
2016-08-21 16:18:22 +0200

    Merge demo page update into docs, remove demo folder

--- docs/javascripts/main.js
+++ docs/javascripts/main.js
@@ -1,31 +1,30 @@
 $(document).ready(function() {
-
     $('a[data-imagelightbox="a"]').imageLightbox({
         activity: true
     });
 
     $('a[data-imagelightbox="b"]').imageLightbox({
-        activity: true,
         overlay: true
     });
 
     $('a[data-imagelightbox="c"]').imageLightbox({
-        activity: true,
         button: true
     });
 
     $('a[data-imagelightbox="d"]').imageLightbox({
-        activity: true,
         caption: true
     });
 
     $('a[data-imagelightbox="e"]').imageLightbox({
-        activity: true,
         navigation: true,
         selector: 'a[data-imagelightbox="e"]'
     });
 
     $('a[data-imagelightbox="f"]').imageLightbox({
+        arrows: true
+    });
+
+    $('a[data-imagelightbox="g"]').imageLightbox({
         activity: true,
         button: true,
         caption: true,
@@ -34,14 +33,9 @@ $(document).ready(function() {
         selector: 'a[data-imagelightbox="f"]'
     });
 
-    var gallery = $('a[data-imagelightbox="g"]').imageLightbox();
+    var gallery = $('a[data-imagelightbox="h"]').imageLightbox();
     $('.trigger-button').on('click', function () {
         gallery.startImageLightbox();
-    });
-
-    $('a[data-imagelightbox="h"]').imageLightbox({
-        activity: true,
-        arrows: true
     });
 
     var instanceI =  $('a[data-imagelightbox="i"]').imageLightbox();
@@ -65,6 +59,7 @@ $(document).ready(function() {
             .attr("src", "images/5.jpg")
             .attr("alt", "Sun, grass and hydrant")
             .appendTo( a );
+        li = $('<li></li>').appendTo( adding_ul );
         a = $("<a></a>")
             .attr('data-imagelightbox',"add")
             .attr('href', "http://osvaldas.info/examples/image-lightbox-responsive-touch-friendly/full/6.jpg")
@@ -75,6 +70,6 @@ $(document).ready(function() {
             .appendTo( a );
 
         // dynamically adding
-        instanceI.addImageLightbox( $("a[data-imagelightbox='add']") );
+        instanceI.addToImageLightbox( $("a[data-imagelightbox='add']") );
     });
 });
