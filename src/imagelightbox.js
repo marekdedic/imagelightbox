ffb0fb2e7ecc6cb8cf18b3b471f96c83165f4ca5
Chris <chrisdavidramos@gmail.com>
2016-10-18 10:44:25 -0400

    imagelightbox sets

--- src/imagelightbox.js
+++ src/imagelightbox.js
@@ -24,6 +24,9 @@
         $overlayObject = $('<div/>', {
             id:'imagelightbox-overlay'
         }),
+        $navItem = $('<a/>', {
+            href:'#',class:"imagelightbox-navitem"
+        }),
         $navObject = $('<div/>', {
             id: 'imagelightbox-nav'
         }),
@@ -118,6 +121,7 @@
                     }
                 },
                 onEnd: function () {
+                    targets = $([]);
                     $wrapper.remove().find("*").remove();
                     if (options.lockBody) {
                         lockBody(false);
@@ -215,10 +219,10 @@
                 $captionObject.html("&nbsp;");
             },
             navigationOn = function (instance, selector) {
-                var images = $(selector);
+                var images = targets;
                 if (images.length) {
                     for (var i = 0; i < images.length; i++) {
-                        $navObject.append($('<a/>',{href:'#'}));
+                        $navObject.append($navItem.clone());
                     }
                     $wrapper.append($navObject);
                     $navObject.on('click.ilb7 touchend.ilb7', function () {
@@ -246,7 +250,7 @@
             navigationUpdate = function (selector) {
                 var items = $navObject.find('a');
                 items.removeClass('active');
-                items.eq($(selector).filter('[href="' + $('#imagelightbox').attr('src') + '"]').index(selector)).addClass('active');
+                items.eq(targets.index(target)).addClass('active');
             },
             arrowsOn = function () {
                 $wrapper.append($arrows);
@@ -260,7 +264,7 @@
                     return false;
                 });
             },
-
+            targetSet = "",
             targets = $([]),
             target = $(),
             image = $(),
@@ -487,14 +491,21 @@
             },
 
             _addTargets = function( newTargets ) {
-                newTargets.each(function () {
-                    targets = targets.add($(this));
-                });
-
-                newTargets.on('click.ilb7', function (e) {
+                newTargets.on('click.ilb7', {set: targetSet}, function (e) {
                     e.preventDefault();
+                    targetSet = $(e.currentTarget).data("imagelightbox");
+                    filterTargets();
                     _openImageLightbox($(this));
                 });
+                function filterTargets () {
+                    newTargets
+                        .filter(function () {
+                            return $(this).data("imagelightbox") === targetSet;
+                        })
+                        .each(function () {
+                            targets = targets.add($(this));
+                        });
+                }
             };
 
         this.startImageLightbox = function () {
@@ -502,6 +513,7 @@
                 _openImageLightbox($(this[0]));
             }
         };
+
 
         $(window).on('resize.ilb7', _setImage);
 
