d98b2d3166d96dd9002993683cd686e12c9717eb
rejas <github@veeck.de>
2016-11-21 18:30:29 +0100

    Add simple click test

--- test/src/ilb2.js
+++ test/src/ilb2.js
@@ -1,9 +1,21 @@
+function openDemo (browser) {
+    browser.url("http://localhost:8080/docs/index.html")
+}
+
 module.exports = {
 
     "Open demo page" : function (browser) {
-        browser
-            .url("http://localhost:8080/docs/index.html")
-            .waitForElementVisible('body', 1000)
+        openDemo(browser);
+        browser.assert.elementPresent("#main_content")
+            .end();
+    },
+
+    "Open lightbox page" : function (browser) {
+        openDemo(browser);
+        browser.click('.options_activity li a')
+            .waitForElementVisible('#imagelightbox', 1000)
+            .assert.elementPresent("#imagelightbox")
+            //.assert.containsText('ol#rso li:first-child', 'Rembrandt - Wikipedia')
             .end();
     }
 };
