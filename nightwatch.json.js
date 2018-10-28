1c054d9b8f3941001e91fe21925853d467f71538
GenaBitu <genabitu@gmail.com>
2018-10-28 21:20:14 +0100

    selenium and chromedriver managed by NPM

--- nightwatch.json.js
+++ nightwatch.json.js
@@ -1,14 +1,9 @@
-module.exports = (function (settings) {
-    if (process.platform === 'linux') {
-        settings.selenium.cli_args['webdriver.chrome.driver'] = 'test/resources/chromedriver-2.43-linux';
-    }
-    else if (process.platform === 'darwin') {
-        settings.selenium.cli_args['webdriver.chrome.driver'] = 'test/resources/chromedriver-2.43-macos';
-    }
-    else if (process.platform === 'win32') {
-        settings.selenium.cli_args['webdriver.chrome.driver'] = 'test/resources/chromedriver-2.43.exe';
-    }
+var seleniumServer = require('selenium-server');
+var chromedriver = require('chromedriver');
 
+module.exports = (function (settings) {
+    settings.selenium.server_path = seleniumServer.path;
+    settings.selenium.cli_args['webdriver.chrome.driver'] = chromedriver.path;
     return settings;
 
 })(require('./nightwatch.json'));
