c5bffae008d1959d493456f45dff5d641a69f46e
Veeck <github@veeck.de>
2019-03-20 18:55:58 +0200

    Try out pupeteer and mocha

--- test/src/ilb2.js
+++ test/src/ilb2.js
@@ -1,3 +1,61 @@
+const { expect } = require('chai');
+
+const express = require('express');
+const app = express();
+
+// Some random port to use for testing
+const PORT = 8080;
+
+// Helper functions to start/stop app before/after tests
+let server = null;
+const startApp = () => {
+    app.use(express.static(__dirname + '/../../docs'));
+    server = app.listen(PORT);
+};
+const stopApp = () => {
+    server.close();
+};
+
+describe('sample test', function () {
+    let page;
+
+    before (async function () {
+        startApp();
+        page = await browser.newPage();
+        await page.goto('http://localhost:8080/');
+    });
+
+    after (async function () {
+        await page.close();
+        stopApp();
+    });
+
+    it('should have the correct page title', async function () {
+        expect(await page.title()).to.eql('Imagelightbox');
+    });
+
+    /*
+    it('should have a heading', async function () {
+        const HEADING_SELECTOR = 'h1';
+        let heading;
+
+        await page.waitFor(HEADING_SELECTOR);
+        heading = await page.$eval(HEADING_SELECTOR, heading => heading.innerText);
+
+        expect(heading).to.eql('Page Title');
+    });
+
+    it('should have a single content section', async function () {
+        const BODY_SELECTOR = '.main-content';
+
+        await page.waitFor(BODY_SELECTOR);
+
+        expect(await page.$$(BODY_SELECTOR)).to.have.lengthOf(1);
+    });
+    */
+});
+
+/*
 function openDemo (browser, hash) {
     var url = 'http://localhost:8080/docs/index.html';
     if (hash)
@@ -83,3 +141,4 @@ module.exports = {
         closeDemo(browser);
     }
 };
+*/
