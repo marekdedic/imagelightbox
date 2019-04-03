4be08e9368e672fe89ccc1f0234d019d62b8f7a6
Marek Dědič <developer@dedic.eu>
2019-04-03 13:30:53 +0200

    Clicking in center of image goes to next

--- docs/javascripts/imagelightbox.js
+++ docs/javascripts/imagelightbox.js
@@ -517,7 +517,7 @@
                             return true;
                         }
                         var posX = (e.pageX || e.originalEvent.pageX) - e.target.offsetLeft;
-                        if (e.target.width / 2 > posX) {
+                        if (e.target.width / 3 > posX) {
                             _previousTarget();
                         } else {
                             _nextTarget();
