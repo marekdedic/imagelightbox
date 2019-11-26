693855ad7e76eb829acef51488d4ae9f882ac537
Marek Dědič <developer@dedic.eu>
2019-11-27 00:35:39 +0100

    Added full ILBOptions types

--- src/interfaces/ILBOptions.ts
+++ src/interfaces/ILBOptions.ts
@@ -1,1 +1,22 @@
-interface ILBOptions {}
+interface ILBOptions {
+    selector: string;
+    id: string;
+    allowedTypes: string;
+    animationSpeed: number;
+    activity: boolean;
+    arrows: boolean;
+    button: boolean;
+    caption: boolean;
+    enableKeyboard: boolean;
+    history: boolean;
+    fullscreen: boolean;
+    gutter: number;
+    offsetY: number;
+    navigation: boolean;
+    overlay: boolean;
+    preloadNext: boolean;
+    quitOnEnd: boolean;
+    quitOnImgClick: boolean;
+    quitOnDocClick: boolean;
+    quitOnEscKey: boolean;
+}
