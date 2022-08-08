1918b799bedc5f7b226d4fec473f14d3b89f0010
Marek Dědič <developer@dedic.eu>
2022-08-09 01:46:57 +0200

    Prettier fix of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,73 @@
 /* eslint-env node */
 
-var gulp            = require('gulp'),
-    autoprefixer    = require('gulp-autoprefixer'),
-    cleanCSS        = require('gulp-clean-css'),
-    concat          = require('gulp-concat'),
-    connect         = require('gulp-connect'),
-    rename          = require('gulp-rename'),
-    ts              = require('gulp-typescript'),
-    uglify          = require('gulp-uglify');
+var gulp = require("gulp"),
+    autoprefixer = require("gulp-autoprefixer"),
+    cleanCSS = require("gulp-clean-css"),
+    concat = require("gulp-concat"),
+    connect = require("gulp-connect"),
+    rename = require("gulp-rename"),
+    ts = require("gulp-typescript"),
+    uglify = require("gulp-uglify");
 
-gulp.task('copy:css', function() {
-    return gulp.src('src/imagelightbox.css')
-        .pipe(gulp.dest('docs/stylesheets/'));
+gulp.task("copy:css", function () {
+    return gulp
+        .src("src/imagelightbox.css")
+        .pipe(gulp.dest("docs/stylesheets/"));
 });
 
-gulp.task('minify:css', gulp.series('copy:css', function() {
-    return gulp.src('src/imagelightbox.css')
-        .pipe(autoprefixer({
-            cascade: false
-        }))
-        .pipe(cleanCSS())
-        .pipe(rename('imagelightbox.min.css'))
-        .pipe(gulp.dest('dist/'));
-}));
+gulp.task(
+    "minify:css",
+    gulp.series("copy:css", function () {
+        return gulp
+            .src("src/imagelightbox.css")
+            .pipe(
+                autoprefixer({
+                    cascade: false,
+                })
+            )
+            .pipe(cleanCSS())
+            .pipe(rename("imagelightbox.min.css"))
+            .pipe(gulp.dest("dist/"));
+    })
+);
 
-gulp.task('copy:js', function() {
-    var tsProject = ts.createProject('tsconfig.json');
-    return gulp.src(['src/**/*.ts'])
-        .pipe(tsProject()).js
-        .pipe(concat('imagelightbox.js'))
-        .pipe(gulp.dest('docs/javascripts/'));
+gulp.task("copy:js", function () {
+    var tsProject = ts.createProject("tsconfig.json");
+    return gulp
+        .src(["src/**/*.ts"])
+        .pipe(tsProject())
+        .js.pipe(concat("imagelightbox.js"))
+        .pipe(gulp.dest("docs/javascripts/"));
 });
 
-gulp.task('minify:js', gulp.series('copy:js', function() {
-    var tsProject = ts.createProject('tsconfig.json');
-    return gulp.src(['src/**/*.ts'])
-        .pipe(tsProject()).js
-        .pipe(concat('imagelightbox.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/'));
-}));
+gulp.task(
+    "minify:js",
+    gulp.series("copy:js", function () {
+        var tsProject = ts.createProject("tsconfig.json");
+        return gulp
+            .src(["src/**/*.ts"])
+            .pipe(tsProject())
+            .js.pipe(concat("imagelightbox.min.js"))
+            .pipe(uglify())
+            .pipe(gulp.dest("dist/"));
+    })
+);
 
-gulp.task('build', gulp.parallel('minify:css', 'minify:js'));
+gulp.task("build", gulp.parallel("minify:css", "minify:js"));
 
-gulp.task('watch', function(done) {
-    gulp.watch(['docs/*.html','src/**/*'], gulp.series('build'));
+gulp.task("watch", function (done) {
+    gulp.watch(["docs/*.html", "src/**/*"], gulp.series("build"));
     done();
 });
 
-gulp.task('dev', gulp.parallel('build', 'watch', function(done) {
-    connect.server({
-        livereload: true
-    });
-    done();
-}));
+gulp.task(
+    "dev",
+    gulp.parallel("build", "watch", function (done) {
+        connect.server({
+            livereload: true,
+        });
+        done();
+    })
+);
 
-gulp.task('default', gulp.series('build'));
+gulp.task("default", gulp.series("build"));
