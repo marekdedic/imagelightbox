e9931a9971851ed93ee95f47a14d00f262dcd098
Marek Dědič <developer@dedic.eu>
2022-08-09 12:51:48 +0200

    Reworked CSS building to output unminified version as well

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,35 @@
 /* eslint-env node */
 
-var gulp = require("gulp"),
-    autoprefixer = require("gulp-autoprefixer"),
-    cleanCSS = require("gulp-clean-css"),
-    concat = require("gulp-concat"),
-    connect = require("gulp-connect"),
-    rename = require("gulp-rename"),
-    ts = require("gulp-typescript"),
-    uglify = require("gulp-uglify");
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const concat = require("gulp-concat");
+const connect = require("gulp-connect");
+const rename = require("gulp-rename");
+const ts = require("gulp-typescript");
+const uglify = require("gulp-uglify");
 
-gulp.task("copy:css", function () {
+gulp.task("build:css:copy", function () {
     return gulp
         .src("src/imagelightbox.css")
+        .pipe(
+            autoprefixer({
+                cascade: false,
+            })
+        )
+        .pipe(gulp.dest("dist"))
         .pipe(gulp.dest("docs/stylesheets/"));
 });
 
-gulp.task(
-    "minify:css",
-    gulp.series("copy:css", function () {
-        return gulp
-            .src("src/imagelightbox.css")
-            .pipe(
-                autoprefixer({
-                    cascade: false,
-                })
-            )
-            .pipe(cleanCSS())
-            .pipe(rename("imagelightbox.min.css"))
-            .pipe(gulp.dest("dist/"));
-    })
-);
+gulp.task("build:css:minify", function () {
+    return gulp
+        .src("dist/imagelightbox.css")
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("build:css", gulp.series("build:css:copy", "build:css:minify"));
 
 gulp.task("copy:js", function () {
     var tsProject = ts.createProject("tsconfig.json");
@@ -53,7 +53,7 @@ gulp.task(
     })
 );
 
-gulp.task("build", gulp.parallel("minify:css", "minify:js"));
+gulp.task("build", gulp.parallel("build:css", "minify:js"));
 
 gulp.task("watch", function (done) {
     gulp.watch(["docs/*.html", "src/**/*"], gulp.series("build"));
