d04b7d34aa451e4b6812c43847b717882a4d2bf0
Marek Dědič <developer@dedic.eu>
2022-10-20 14:17:29 +0200

    Enabled build minification

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         filename: "[name].js",
     },
     optimization: {
-        minimize: false,
+        minimize: true,
         minimizer: [
             new TerserPlugin({
                 extractComments: false,
