8947a0b0c7111ad5b9d2921d38f3eef02d1b0b99
Marek Dědič <developer@dedic.eu>
2022-10-20 14:17:44 +0200

    Removed old build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,8 @@
 const gulp = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
-const concat = require("gulp-concat");
 const connect = require("gulp-connect");
 const rename = require("gulp-rename");
-const ts = require("gulp-typescript");
-const uglify = require("gulp-uglify");
 const webpack = require("webpack-stream");
 
 gulp.task("build:css:copy", function () {
@@ -31,26 +28,6 @@ gulp.task("build:css:minify", function () {
 });
 
 gulp.task("build:css", gulp.series("build:css:copy", "build:css:minify"));
-
-gulp.task("build:js:copy", function () {
-    const tsProject = ts.createProject("tsconfig.json");
-    return gulp
-        .src(["src/**/*.ts", "types/**/*.ts"])
-        .pipe(tsProject())
-        .js.pipe(concat("imagelightbox.js"))
-        .pipe(gulp.dest("dist/"))
-        .pipe(gulp.dest("docs/javascripts/"));
-});
-
-gulp.task("build:js:minify", function () {
-    return gulp
-        .src("dist/imagelightbox.js")
-        .pipe(uglify())
-        .pipe(rename({ suffix: ".min" }))
-        .pipe(gulp.dest("dist/"));
-});
-
-//gulp.task("build:js", gulp.series("build:js:copy", "build:js:minify"));
 
 gulp.task("build:js", function () {
     return gulp
