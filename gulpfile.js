ba8e24e562ea4736696519405605bed35216bcb2
Marek Dědič <developer@dedic.eu>
2023-10-25 18:36:15 +0200

    Using arrow functions

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,8 @@ const named = require("vinyl-named");
 const rename = require("gulp-rename");
 const webpack = require("webpack-stream");
 
-gulp.task("build:css:copy", function () {
-    return gulp
+gulp.task("build:css:copy", () =>
+    gulp
         .src("src/imagelightbox.css")
         .pipe(
             autoprefixer({
@@ -17,38 +17,38 @@ gulp.task("build:css:copy", function () {
             }),
         )
         .pipe(gulp.dest("dist/"))
-        .pipe(gulp.dest("docs/stylesheets/"));
-});
+        .pipe(gulp.dest("docs/stylesheets/")),
+);
 
-gulp.task("build:css:minify", function () {
-    return gulp
+gulp.task("build:css:minify", () =>
+    gulp
         .src("dist/imagelightbox.css")
         .pipe(cleanCSS())
         .pipe(rename({ suffix: ".min" }))
-        .pipe(gulp.dest("dist/"));
-});
+        .pipe(gulp.dest("dist/")),
+);
 
 gulp.task("build:css", gulp.series("build:css:copy", "build:css:minify"));
 
-gulp.task("build:js", function () {
-    return gulp
+gulp.task("build:js", () =>
+    gulp
         .src("src/imagelightbox.ts")
         .pipe(named((file) => file.stem + ".min"))
         .pipe(webpack(require("./webpack.config.js")))
         .pipe(gulp.dest("dist/"))
-        .pipe(gulp.dest("docs/javascripts/"));
-});
+        .pipe(gulp.dest("docs/javascripts/")),
+);
 
 gulp.task("build", gulp.parallel("build:css", "build:js"));
 
-gulp.task("watch", function (done) {
+gulp.task("watch", (done) => {
     gulp.watch(["docs/*.html", "src/**/*"], gulp.series("build"));
     done();
 });
 
 gulp.task(
     "dev",
-    gulp.parallel("build", "watch", function (done) {
+    gulp.parallel("build", "watch", (done) => {
         connect.server({
             livereload: true,
         });
