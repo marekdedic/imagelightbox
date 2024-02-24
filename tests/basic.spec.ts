cf41f4c6b69bf148e9ee6a26067a7c8db7f930d4
Marek Dědič <developer@dedic.eu>
2024-02-25 00:13:17 +0100

    Converted basic heading test to playwright

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,5 +1,8 @@
 import { expect, test } from "playwright-test-coverage";
 
-test("works with basic configuration", async ({ page }) => {
-  await page.goto("/");
+test("has correct title", async ({ page }) => {
+    await page.goto("/");
+    await expect(
+        page.getByRole("heading").getByText("Imagelightbox"),
+    ).toBeVisible();
 });
