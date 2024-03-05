2054aaedef49e031f23e740cf8cb6738d6b198c7
Marek Dědič <developer@dedic.eu>
2024-03-05 16:58:23 +0100

    Fixed type declaration code style

--- types/imagelightbox.d.ts
+++ types/imagelightbox.d.ts
@@ -1,31 +1,31 @@
 interface ILBOptions {
-    selector: string;
-    allowedTypes: string;
-    animationSpeed: number;
-    activity: boolean;
-    arrows: boolean;
-    button: boolean;
-    caption: boolean;
-    enableKeyboard: boolean;
-    history: boolean;
-    fullscreen: boolean;
-    gutter: number;
-    offsetY: number;
-    navigation: boolean;
-    overlay: boolean;
-    preloadNext: boolean;
-    quitOnEnd: boolean;
-    quitOnImgClick: boolean;
-    quitOnDocClick: boolean;
-    quitOnEscKey: boolean;
+  selector: string;
+  allowedTypes: string;
+  animationSpeed: number;
+  activity: boolean;
+  arrows: boolean;
+  button: boolean;
+  caption: boolean;
+  enableKeyboard: boolean;
+  history: boolean;
+  fullscreen: boolean;
+  gutter: number;
+  offsetY: number;
+  navigation: boolean;
+  overlay: boolean;
+  preloadNext: boolean;
+  quitOnEnd: boolean;
+  quitOnImgClick: boolean;
+  quitOnDocClick: boolean;
+  quitOnEscKey: boolean;
 }
 
 interface JQuery {
-    imageLightbox(opts?: Partial<ILBOptions>): JQuery;
-    addToImageLightbox(elements: JQuery): void;
-    openHistory(): void;
-    loadPreviousImage(): void;
-    loadNextImage(): void;
-    quitImageLightbox(): void;
-    startImageLightbox(element?: JQuery): void;
+  imageLightbox(opts?: Partial<ILBOptions>): JQuery;
+  addToImageLightbox(elements: JQuery): void;
+  openHistory(): void;
+  loadPreviousImage(): void;
+  loadNextImage(): void;
+  quitImageLightbox(): void;
+  startImageLightbox(element?: JQuery): void;
 }
