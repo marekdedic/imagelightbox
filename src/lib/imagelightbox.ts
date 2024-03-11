1a97928e07ca3f769521f0b524cb436ddc018eb4
Marek Dědič <developer@dedic.eu>
2024-03-11 15:00:49 +0000

    Cleaned up old state

--- src/lib/imagelightbox.ts
+++ src/lib/imagelightbox.ts
@@ -39,88 +39,6 @@ $.fn.imageLightbox = function (opts?: Partial<ILBOptions>): JQuery {
     $(this).data("imagelightbox") as string | undefined,
     $(this),
   );
-  let inProgress = false; // Whether a transition is in progress
-  let target = $(); // targets.eq(targetIndex)
-  let targetIndex = -1; // The index of the currently open image in its set (targets). -1 if the lightbox isn't open
-  let targets: JQuery = $([]); // Clickable images
-  const _quitImageLightbox = (): void => {
-      state.closeLightbox();
-      targetIndex = -1;
-      $("body").removeClass("ilb-open");
-    },
-    _previousTarget = (): void => {
-      state.previousImage();
-      if (inProgress) {
-        return;
-      }
-
-      targetIndex--;
-      if (targetIndex < 0) {
-        if (options.quitOnEnd) {
-          _quitImageLightbox();
-          return;
-        } else {
-          targetIndex = targets.length - 1;
-        }
-      }
-      target = targets.eq(targetIndex);
-    },
-    _nextTarget = (): void => {
-      state.nextImage();
-      if (inProgress) {
-        return;
-      }
-
-      targetIndex++;
-      if (targetIndex >= targets.length) {
-        if (options.quitOnEnd) {
-          _quitImageLightbox();
-          return;
-        } else {
-          targetIndex = 0;
-        }
-      }
-      target = targets.eq(targetIndex);
-    },
-    _openImageLightbox = ($target: JQuery): void => {
-      state.openLightboxWithImage($target);
-      if (inProgress) {
-        return;
-      }
-      inProgress = false;
-      target = $target;
-      targetIndex = targets.index(target);
-      $("body").addClass("ilb-open");
-    },
-    isTargetValid = (element: JQuery): boolean =>
-      (($(element).prop("tagName") as string).toLowerCase() === "a" &&
-        new RegExp(".(" + options.allowedTypes + ")$", "i").test(
-          $(element).attr("href")!,
-        )) ||
-      $(element).data("ilb2Video") !== undefined,
-    _addTargets = function (newTargets: JQuery): void {
-      function filterTargets(): void {
-        newTargets
-          .filter(function (): boolean {
-            return isTargetValid($(this));
-          })
-          .each(function (): void {
-            targets = targets.add($(this));
-          });
-      }
-      newTargets.each(function (): void {
-        targets = newTargets.add($(this));
-      });
-      newTargets.on("click.ilb7", function (e): void {
-        e.preventDefault();
-        filterTargets();
-        if (targets.length < 1) {
-          _quitImageLightbox();
-        } else {
-          _openImageLightbox($(this));
-        }
-      });
-    };
 
   function toggleFullScreen(): void {
     const doc = window.document as LegacyDocument;
@@ -148,7 +66,7 @@ $.fn.imageLightbox = function (opts?: Partial<ILBOptions>): JQuery {
           !$(e.target).is(state.temp_getImageView()!.temp_getImage())
         ) {
           e.preventDefault();
-          _quitImageLightbox();
+          state.closeLightbox();
         }
       });
     }
@@ -178,17 +96,17 @@ $.fn.imageLightbox = function (opts?: Partial<ILBOptions>): JQuery {
         if ([27].includes(e.which!) && options.quitOnEscKey) {
           e.stopPropagation();
           e.preventDefault();
-          _quitImageLightbox();
+          state.closeLightbox();
         }
         if ([37].includes(e.which!)) {
           e.stopPropagation();
           e.preventDefault();
-          _previousTarget();
+          state.previousImage();
         }
         if ([39].includes(e.which!)) {
           e.stopPropagation();
           e.preventDefault();
-          _nextTarget();
+          state.nextImage();
         }
       });
     }
@@ -196,10 +114,7 @@ $.fn.imageLightbox = function (opts?: Partial<ILBOptions>): JQuery {
 
   this.addToImageLightbox = (elements: JQuery): void => {
     state.addImages(elements);
-    _addTargets(elements);
   };
-
-  this.addToImageLightbox($(this));
 
   this.openHistory = (): void => {
     if (options.history) {
@@ -210,15 +125,15 @@ $.fn.imageLightbox = function (opts?: Partial<ILBOptions>): JQuery {
   this.openHistory();
 
   this.loadPreviousImage = (): void => {
-    _previousTarget();
+    state.previousImage();
   };
 
   this.loadNextImage = (): void => {
-    _nextTarget();
+    state.nextImage();
   };
 
   this.quitImageLightbox = function (): JQuery {
-    _quitImageLightbox();
+    state.closeLightbox();
     return this;
   };
 
