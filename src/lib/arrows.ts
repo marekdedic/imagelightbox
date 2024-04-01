c9ef8d1b942322df09149b70c2efdc7bf0080b0d
Marek Dědič <developer@dedic.eu>
2024-04-01 18:18:40 +0200

    Removed jQuery from arrows

--- src/lib/arrows.ts
+++ src/lib/arrows.ts
@@ -1,28 +1,44 @@
 import "./arrows.css";
 import "./button.css";
 
-import $ from "jquery";
-
 import { getContainer } from "./container";
 
-const leftArrow = $("<div/>", {
-  class: "ilb-button ilb-arrow",
-  id: "ilb-arrow-left",
-});
-const rightArrow = $("<div/>", {
-  class: "ilb-button ilb-arrow",
-  id: "ilb-arrow-right",
-});
+const leftArrow = document.createElement("div");
+leftArrow.classList.add("ilb-button", "ilb-arrow");
+leftArrow.setAttribute("id", "ilb-arrow-left");
+const rightArrow = document.createElement("div");
+rightArrow.classList.add("ilb-button", "ilb-arrow");
+rightArrow.setAttribute("id", "ilb-arrow-right");
+
+let wrappedLeft: ((e: Event) => void) | null = null;
+let wrappedRight: ((e: Event) => void) | null = null;
 
 export function addArrowsToDOM(onleft: () => void, onright: () => void): void {
-  $(getContainer()).append(
-    leftArrow.on("click.ilb7 touchend.ilb7", (): boolean => {
-      onleft();
-      return false;
-    }),
-    rightArrow.on("click.ilb7 touchend.ilb7", (): boolean => {
-      onright();
-      return false;
-    }),
-  );
+  wrappedLeft = (e): void => {
+    e.stopPropagation();
+    onleft();
+  };
+  wrappedRight = (e): void => {
+    e.stopPropagation();
+    onright();
+  };
+  leftArrow.addEventListener("click", wrappedLeft);
+  leftArrow.addEventListener("touchend", wrappedLeft);
+  rightArrow.addEventListener("click", wrappedRight);
+  rightArrow.addEventListener("touchend", wrappedRight);
+  getContainer().append(leftArrow, rightArrow);
+}
+
+export function removeArrowsFromDOM(): void {
+  leftArrow.remove();
+  if (wrappedLeft !== null) {
+    leftArrow.removeEventListener("click", wrappedLeft);
+    leftArrow.removeEventListener("touchend", wrappedLeft);
+  }
+  if (wrappedRight !== null) {
+    rightArrow.removeEventListener("click", wrappedRight);
+    rightArrow.removeEventListener("touchend", wrappedRight);
+  }
+  wrappedLeft = null;
+  wrappedRight = null;
 }
