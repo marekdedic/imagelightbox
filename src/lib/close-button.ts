34a2f7ad19c8acc8595305dcbb9780bf35351986
Marek Dědič <developer@dedic.eu>
2024-04-01 19:03:00 +0200

    Removed jQuery from close button

--- src/lib/close-button.ts
+++ src/lib/close-button.ts
@@ -1,20 +1,26 @@
 import "./button.css";
 import "./close-button.css";
 
-import $ from "jquery";
-
 import { getContainer } from "./container";
 
-const closeButton = $("<button/>", {
-  class: "ilb-button",
-  id: "ilb-close-button",
-});
+const closeButton = document.createElement("button");
+closeButton.classList.add("ilb-button");
+closeButton.setAttribute("id", "ilb-close-button");
+
+let wrappedClose: ((e: Event) => void) | null = null;
 
 export function addCloseButtonToDOM(onclick: () => void): void {
-  $(getContainer()).append(
-    closeButton.on("click.ilb7", (): boolean => {
-      onclick();
-      return false;
-    }),
-  );
+  wrappedClose = (e): void => {
+    e.stopPropagation();
+    onclick();
+  };
+  closeButton.addEventListener("click", wrappedClose);
+  getContainer().appendChild(closeButton);
+}
+
+export function removeCloseButtonFromDOM(): void {
+  if (wrappedClose !== null) {
+    closeButton.removeEventListener("click", wrappedClose);
+  }
+  closeButton.remove();
 }
