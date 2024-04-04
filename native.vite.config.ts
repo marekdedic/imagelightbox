e5a3775a29c02d6912279a8917788a141a28a339
Marek Dědič <developer@dedic.eu>
2024-04-04 17:16:31 +0200

    Building native and jquery outputs in parallel

---
+++ native.vite.config.ts
@@ -0,0 +1,25 @@
+/* eslint-env node */
+
+import { resolve } from "path";
+import { webpackStats } from "rollup-plugin-webpack-stats";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [webpackStats()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/lib/imagelightbox.ts"),
+      name: "imagelightbox",
+      fileName: "imagelightbox",
+    },
+    outDir: "../dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: "imagelightbox.[ext]",
+      },
+    },
+  },
+  root: "src",
+  publicDir: false,
+});
