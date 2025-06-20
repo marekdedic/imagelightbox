031c8621f7ec772861cfc74b08ac89a8d1844d8c
Marek Dědič <developer@dedic.eu>
2025-06-20 10:17:45 +0200

    Linting JS files with typescript-eslint as well

--- eslint.config.js
+++ eslint.config.js
@@ -45,6 +45,8 @@ export default tseslint.config(
       prettierRecommended,
       commentsConfig.recommended,
       compat.configs["flat/recommended"],
+      tseslint.configs.strict,
+      tseslint.configs.stylistic,
       perfectionist.configs["recommended-natural"],
     ],
     files: ["**/*.js", "**/*.ts"],
